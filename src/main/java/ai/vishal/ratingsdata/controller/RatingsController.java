b48912686073dcc40309e4ff3e0bd658be95f2c6
vishal2468 <vishalpoddar2468@gmail.com>
2023-02-11 12:27:28 +0530

    removed unused imports

--- src/main/java/ai/vishal/ratingsdata/controller/RatingsController.java
+++ src/main/java/ai/vishal/ratingsdata/controller/RatingsController.java
@@ -1,10 +1,8 @@
 package ai.vishal.ratingsdata.controller;
-import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import ai.vishal.ratingsdata.model.Rating;
 import ai.vishal.ratingsdata.model.UserRating;
 import ai.vishal.ratingsdata.repository.UserRatingRepo;
 
